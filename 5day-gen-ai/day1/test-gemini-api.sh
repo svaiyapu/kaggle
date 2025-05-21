21240a68427c87ade9aefdff83eb171d4408d67d
Senthil Vaiyapuri <svaiyapuri@gmail.com>
2025-05-21 11:49:29 -0700

    5day gen ai course

---
+++ 5day-gen-ai/day1/test-gemini-api.sh
@@ -0,0 +1,16 @@
+set -x
+
+curl "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=__APU_KDY__" \
+  -H 'Content-Type: application/json' \
+  -X POST \
+  -d '{
+    "contents": [
+      {
+        "parts": [
+          {
+            "text": "Explain how AI works in a few words"
+          }
+        ]
+      }
+    ]
+  }'
